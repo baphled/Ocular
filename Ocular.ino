9784184a7d1cf3c882ee2e49298e2e28f6f48ab4
Yomi Colledge <baphled@boodah.net>
2013-06-26 05:06:33 +0100

    Change menu layout

--- Ocular.ino
+++ Ocular.ino
@@ -210,17 +210,17 @@ void displayHelp() {
   // TODO: Refactor so that we can scroll through menu
 	clearScreen();
 	lcd.setCursor(0, 1);
-	lcd.print("#1 Deploys");
+	lcd.print("1 Deploys");
 	lcd.setCursor(10, 1);
-	lcd.print("#2 Commits");
+	lcd.print("2 Commits");
 	lcd.setCursor(0, 2);
-	lcd.print("#3 Errors");
+	lcd.print("3 Errors");
 	lcd.setCursor(10, 2);
-	lcd.print("#4 Stats");
+	lcd.print("4 Stats");
 	lcd.setCursor(0, 3);
-	lcd.print("#5 Repos");
+	lcd.print("5 Repos");
 	lcd.setCursor(10, 3);
-	lcd.print("#0 Help");
+	lcd.print("0 Help");
 }
 
  /*
